e88957490b017364b0cf20593615c96b67c5a2d2
Óscar Llorente <llorente_oscar@hotmail.com>
2024-06-23 19:16:34 -0500

    Creando archivo

---
+++ components/UI/Footer/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="w-100 bg-dark text-white p-3">
+    <footer class="container">
+      [FOOTER]
+    </footer>
+  </footer>
+</template>
---
+++ pages/categorias/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const title = "Categorías"
+const error = ref()
+const isError = ref(false)
+const loading = ref(false)
+const limit = ref(10)
+const page = ref(1)
+const items = ref<any[]>([])
+
+const getItems = async () => {
+  loading.value = true
+
+  try {
+    items.value = []
+  } catch (error: any) {
+    isError.value = true
+    error.value = error
+  } finally {
+    loading.value = false
+  }
+}
+
+onMounted(() => getItems())
+</script>
+
+<template>
+
+  <Head>
+    <Title>{{ title }}</Title>
+  </Head>
+
+  <NuxtLayout class="">
+    <h1>{{ title }}</h1>
+
+    <UIMain :error="error" :isError="isError" :loading="loading">
+      <pre>{{ items }}</pre>
+    </UIMain>
+  </NuxtLayout>
+</template>
