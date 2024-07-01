e003a768924ac6da7403023379bdc54e4bddf161
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-01 14:04:33 -0500

    Actualizando

--- components/UI/Footer/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <footer class="w-100 bg-dark text-white p-3">
-    <footer class="container">
-      [FOOTER]
-    </footer>
-  </footer>
-</template>
